37761a2ccec21842e4fd5817a3ae6b369e72eb91
Dashcloud-ML <dwagh0203@gmmail.com>
2023-04-02 10:42:49 +0530

    initial commit message

--- App.js
+++ App.js
@@ -1,20 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button } from 'react-native';
+import DatePicker from 'react-native-datepicker';
+import firebase from 'firebase';
 
-export default function App() {
+const Page1 = ({ navigation }) => {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [dob, setDob] = useState(new Date());
+  const [occupation, setOccupation] = useState('');
+  const [company, setCompany] = useState('');
+
+  const handleSave = () => {
+    firebase.firestore().collection('users').add({
+      firstName,
+      lastName,
+      dob,
+      occupation,
+      company,
+    })
+    .then(() => {
+      navigation.navigate('Page2', {
+        firstName,
+        lastName,
+        dob,
+        occupation,
+        company,
+      });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Text>First Name</Text>
+      <TextInput value={firstName} onChangeText={setFirstName} />
+      <Text>Last Name</Text>
+      <TextInput value={lastName} onChangeText={setLastName} />
+      <Text>Date of Birth</Text>
+      <DatePicker
+        date={dob}
+        onDateChange={setDob}
+        mode="date"
+      />
+      <Text>Occupation</Text>
+      <TextInput value={occupation} onChangeText={setOccupation} />
+      <Text>Company</Text>
+      <TextInput value={company} onChangeText={setCompany} />
+      <Button title="Save" onPress={handleSave} />
     </View>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default Page1;
